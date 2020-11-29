5315dda2bb7dab90ee8553920644c84cfaaed7a6
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-29 23:31:46 +0900

    途中

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express =require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+app.disable("x-powered-by");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use("/", require("./routes/index.js"));
+
+
+app.listen(3001);
+
