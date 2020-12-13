fdd4bb8f0adf89c270fef2258e4981695d84e505
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-13 15:22:24 +0900

    システムログ出力の実装

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const systemlogger = require("./lib/log/systemlogger.js");
 const express = require("express");
 const app = express();
+
 
 app.set("view engine", "ejs");
 
@@ -8,5 +10,7 @@ app.disable("x-powered-by");
 app.use("/public", express.static(__dirname + "/public/" + (process.env.NODE_ENV === "development" ? "development" : "production")));
 
 app.use("/" , require("./routes/index.js"));
+
+app.use(systemlogger());
 
 app.listen(3000);
---
+++ config/log4js.config.js
@@ -0,0 +1,27 @@
+// pathを結合するため。pathのミドルウェアを使う
+const path = require("path");
+// ルートディレクトリの絶対パスを取得する
+const ROOT = path.join(__dirname, "../");
+module.exports = {
+  appenders: {
+    ConsoleLogAppender: {
+      type: "console"
+    },
+    FileLogAppender: {
+      type: "file",
+      filename: path.join(ROOT, "./log/system/system.log"),
+      maxLogSize: 5000000,
+      bakups: 10,
+    }
+  },
+  categories: {
+    "default": {
+      appenders:["ConsoleLogAppender"],
+      level: "ALL"
+    },
+    system: {
+      appenders: ["FileLogAppender"],
+      level: "ERROR"
+    }
+  }
+};
