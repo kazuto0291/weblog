c947884b846dc2c167f77a8fb196965efde83633
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-13 12:45:33 +0900

    gitignore

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express =require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+
 app.disable("x-powered-by");
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.use("/", require("./routes/index.js"));
